406533fddcd6f3f2e4c96654a21615c5ab0c6aad
Viper4everx <Mhessasta@gmail.com>
2025-11-28 19:47:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -155,7 +155,7 @@ def get_yahoo_data(ticker):
 # ==========================================
 # 4. UI: INPUTS
 # ==========================================
-c_tick, c_space, c_pdf = st.columns([.50, 5, 0.55], vertical_alignment="bottom")
+c_tick, c_space, c_pdf = st.columns([.50, 5, 0.45], vertical_alignment="bottom")
 
 with c_tick:
     ticker = st.text_input("Ticker", "NVDA").upper()
